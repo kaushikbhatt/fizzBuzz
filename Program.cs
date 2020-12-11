9d511ee9fa0124dd1849588ddefb6558c177c071
kbhatt488 <kaushikb@vedlogic.com>
2020-12-11 12:56:32 +0530

    fizzbuzz push

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Section2Fizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+    }
+}
